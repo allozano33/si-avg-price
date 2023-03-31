c4ab32fe9abcb1999959004e9dc119370cd5bb3e
danilokadota87 <danilo.kadota@mercadolivre.com>
2023-03-31 15:24:08 -0300

    teste controller

--- app/src/test/kotlin/com/mercadolibre/si_avg_price/controller/AveragePriceControllerTest.kt
+++ app/src/test/kotlin/com/mercadolibre/si_avg_price/controller/AveragePriceControllerTest.kt
@@ -4,6 +4,8 @@ import com.mercadolibre.si_avg_price.config.IntegrationTest
 import com.mercadolibre.si_avg_price.entrypoint.controller.AveragePriceController
 import com.mercadolibre.si_avg_price.facade.ProcessAveragePriceFacade
 import com.mercadolibre.si_avg_price.provider.AverageCostDTOProvider
+import com.mercadolibre.si_avg_price.provider.AveragePriceProcessProvider
+import com.mercadolibre.si_avg_price.provider.SapOutputProvider
 import com.mercadolibre.si_avg_price.utils.loadJsonAsString
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.coEvery
@@ -32,10 +34,52 @@ class AveragePriceControllerTest : IntegrationTest() {
             .json(loadJsonAsString("src/test/resources/__files/entrypoint/controller/average_cost.json"))
     }
 
+    @Test
+    fun `given all average cost - should return from database successfully`() {
+
+        val averageCostDTO = AverageCostDTOProvider.get()
+
+        coEvery {
+            processAveragePriceFacade.getAll()
+        } returns listOf(averageCostDTO)
+
+        webClientGetAll()
+            .expectStatus().isOk
+            .expectBody()
+            .json(loadJsonAsString("src/test/resources/__files/entrypoint/controller/average_cost_list.json"))
+    }
+
+    @Test
+    fun `insert cost - should return from database successfully`() {
+
+        val sapOutput = SapOutputProvider.get()
+        val averagePriceProcess = AveragePriceProcessProvider.get()
+        coEvery {
+            processAveragePriceFacade.execute(averagePriceProcess)
+        } returns sapOutput
+
+        webClientInsert()
+            .expectStatus().isOk
+            .expectBody()
+            .json(loadJsonAsString("src/test/resources/__files/entrypoint/controller/sap_output.json"))
+    }
+
 
     private fun webClientGet() =
         webTestClient.get()
             .uri("/average-price/cnpj/$CNPJ/sku/$SKU")
+            .exchange()
+
+    private fun webClientGetAll() =
+        webTestClient.get()
+            .uri("/average-price/listAll")
+            .exchange()
+
+    private fun webClientInsert() =
+        webTestClient.post()
+            .uri("/average-price/insert")
+            .header("content-type", "application/json")
+            .bodyValue(loadJsonAsString("src/test/resources/__files/entrypoint/controller/controller_insert_average_cost.json"))
             .exchange()
 
 
